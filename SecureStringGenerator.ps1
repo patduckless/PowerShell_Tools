1d991eb2fb51cecdd0f270717eb2959478023ee4
patduckless <51826628+patduckless@users.noreply.github.com>
2019-07-18 09:01:31 +0100

    Added Documentation
    Prior to this no documentation.

--- SecureStringGenerator.ps1
+++ SecureStringGenerator.ps1
@@ -1,1 +1,3 @@
-﻿read-host -assecurestring | convertfrom-securestring | out-file C:\mysecurestring.txt
+# This needs to be run in the user instance and on the machine it will be used on. 
+
+read-host -assecurestring | convertfrom-securestring | out-file C:\mysecurestring.txt
