db1b2ec800695481f3f1bc16fcb8dadc7707e858
patrick duckles <51826628+patduckless@users.noreply.github.com>
2019-10-25 16:27:14 +0100

    Delete remote shutdown.ps1

--- remote shutdown.ps1
+++
@@ -1,7 +0,0 @@
-﻿{
-$username = "Username"
-$password = Get-Content 'C:\Users\Administrator\Desktop\mysecurestring.txt' | ConvertTo-SecureString
-$cred = new-object -typename System.Management.Automation.PSCredential `
-         -argumentlist $username, $password
-         }
-Stop-Computer -ComputerName <computerName> -Credential $cred
