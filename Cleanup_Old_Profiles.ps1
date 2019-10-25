a295d213df55a1228211457f39f8e3d24620e143
patrick duckles <51826628+patduckless@users.noreply.github.com>
2019-10-25 16:16:09 +0100

    Add files via upload

---
+++ Cleanup_Old_Profiles.ps1
@@ -0,0 +1,17 @@
+﻿$loc = Get-Location
+$UserName = $env:UserName
+table -HideTableHeaders | Out-String
+cd HKLM:\
+cd "SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList"
+ls | ForEach-Object{ $Profile=$_.GetValue('ProfileImagePath')
+if ($profile -notmatch "administrator|Ctx_StreamingSvc|NetworkService|Localservice|systemprofile|$Username"){
+Write-Host "$Profile removed"
+Remove-Item $_.PSPath -Recurse -ErrorAction SilentlyContinue
+cd C:\
+Remove-Item $Profile -Recurse -ErrorAction SilentlyContinue
+cd HKLM:\
+ }else{ 
+ Write-Host "Skipping $Profile" 
+ }
+}
+cd $loc
