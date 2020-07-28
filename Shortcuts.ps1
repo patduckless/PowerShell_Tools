f1e4c147a697a022ca6dc89ccc5bc115baa16d56
patrick duckles <51826628+patduckless@users.noreply.github.com>
2020-07-28 08:31:40 +0100

    Added comments. Shortcuts.ps1
    Script lacked comments explaining the functions within the script causing confusion on the use.

--- Shortcuts.ps1
+++ Shortcuts.ps1
@@ -1,3 +1,5 @@
-$path = '\\?\<filePath>'
+# Provide UNC path to share you wish to scan and clean up.
+$path = '\\<server name>\<filePath>'
 $share = Get-ChildItem -LiteralPath $path -Recurse -Filter "*.lnk" | Where { $_.PsIsContainer -eq $False }
+# Remove the WhatIf parameter to actually remove the files, otherwise this will just list them.
 $share |  Remove-Item -ErrorAction SilentlyContinue -WhatIf
