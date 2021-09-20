93a78c6fc3fc51ce61f276ff551e75a135edb0d8
P <51826628+patduckless@users.noreply.github.com>
2021-09-20 16:05:22 +0100

    Update Printer_info_extract.ps1
    Replace deprecated Get-WMIObject, with replacement cmdlet Get-CIMInstance.

--- Printer_info_extract.ps1
+++ Printer_info_extract.ps1
@@ -1,2 +1,2 @@
 ﻿$printserver = "<serverName>" 
-Get-WMIObject -class Win32_Printer -computer $printserver | Select Name,DriverName,PortName | Export-CSV -path 'C:\Users\%username%\Desktop\printers.csv'
+ Get-CimInstance -ClassName Win32_Printer -computer $printserver | Select Name,DriverName,PortName | Export-CSV -path 'C:\Users\%username%\Desktop\printers.csv'
